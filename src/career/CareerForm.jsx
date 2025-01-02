bbe73794241c58d370cdba3a9b4f08b01f42df26
vikraman1653763 <vikraman1653763@gmail.com>
2025-01-02 15:29:44 +0530

    02-01-2025

--- src/career/CareerForm.jsx
+++ src/career/CareerForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { serverUrl } from "../constant";
-import "../style/career.css";
+import "../style/Career.css";
 
 const CareerForm = () => {
 const { id } = useParams();
