9aecdaf98e5c3ca21ec8fdb07a3d4fb785e1aed8
vikraman1653763 <vikraman1653763@gmail.com>
2025-03-08 10:09:06 +0530

    08-03-2025

--- src/product/Survey/Survey.jsx
+++ src/product/Survey/Survey.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import'../s11/s11.css';
+import'../S11/s11.css';
 import Sec1 from '../S11/S11-Spec.jsx';
 import Sec2 from '../S11/S11-Usecase.jsx';
 import Sec3 from '../S11/S11-Techspec.jsx';
