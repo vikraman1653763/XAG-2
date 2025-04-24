afd753cc244db5d43e2cb3fe741a469dd6fafccd
vikraman1653763 <vikraman1653763@gmail.com>
2025-04-24 11:23:20 +0530

    24-04-2025

---
+++ src/product/Rescue/Data.js
@@ -0,0 +1,85 @@
+export const usageData = [
+  {
+    "title": "Search and Rescue",
+    "desc": "Deploy rapid aerial search missions in disaster-stricken areas to locate missing persons with thermal and high-resolution imaging."
+  },
+  {
+    "title": "Disaster Response",
+    "desc": "Assess damage from natural disasters such as earthquakes, floods, and wildfires, enabling faster relief coordination."
+  },
+  {
+    "title": "Medical Supply Delivery",
+    "desc": "Transport critical medical supplies, including first aid kits and life-saving drugs, to inaccessible or high-risk areas."
+  },
+  {
+    "title": "Firefighting Support",
+    "desc": "Enhance firefighting efforts with real-time aerial monitoring, thermal imaging for heat detection, and situational awareness."
+  },
+  {
+    "title": "Hazardous Area Assessment",
+    "desc": "Survey dangerous environments, such as collapsed buildings, radiation zones, or chemical spills, without risking human lives."
+  },
+  {
+    "title": "Maritime Rescue Operations",
+    "desc": "Assist in offshore rescue missions by tracking distressed vessels and deploying flotation devices."
+  }
+];
+
+  
+export const slideData = [
+  {
+    index: 4,
+    headline: "High-Altitude Perimeter Monitoring",
+    src: "/assets/s11/s11-survillence-high.webm",
+  },
+  {
+    index: 0,
+    headline: "Forest Fire Surveillance ",
+    src: "/assets/s11/s11-survillence-fire.mp4",
+  },
+  {
+    index: 1,
+    headline: "Real-Time Crowd Surveillance",
+    src: "/assets/s11/s11-survillence-crowd.webm",
+  },
+  {
+    index: 2,
+    headline: "Real-Time Wildlife Surveillance",
+    button: "Listen",
+    src: "/assets/s11/s11-survillence-animal.webm",
+  },
+  {
+    index: 3,
+    headline: "Heat Signature Tracking for Animal Movements",
+     src: "/assets/s11/s11-survillence-heat.webm",
+  },
+  {
+    index: 5,
+    headline: "Progress Tracking and Safety Assurance for Construction Projects",
+     src: "/assets/s11/s11-survillence-construction.mp4",
+  },
+];
+
+export const introData = {
+  title: "S11-Rescue",
+  subtitle: "Rapid Aerial Support for Critical Emergency Operations",
+  images: [
+    "/assets/s11/s11-xt-10a.webp",
+    "/assets/s11/s11-xt-30.webp",
+    "/assets/s11/s11-xag-lidar.webp",
+
+  ]
+};
+
+
+export const payloadData = {
+  
+  bg:"/assets/s11/s11-pay-survey.webp",
+ items:[
+
+  { name: "Xag-Multi Spectral", image: "/assets/s11/s11-pay-ms.webp" },
+  { name: "XAG-Lidar", image: "/assets/s11/s11-pay-xtlidar.webp" },
+  { name: "Sony Alpha 600", image: "/assets/s11/s11-pay-sony.webp" }
+ ]
+
+};
