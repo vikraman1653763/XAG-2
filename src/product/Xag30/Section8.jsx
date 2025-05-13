827f1ba08c20e4e6895452f1b66ebfdb66ee3555
vikraman1653763 <vikraman1653763@gmail.com>
2025-05-13 17:51:55 +0530

    13-05-2025

--- src/product/Xag30/Section8.jsx
+++ src/product/Xag30/Section8.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 const features = [
   { description: 'Effortless folding mechanism for rapid deployment', video: '/assets/s11/s11-em-fire.webm' },
-  { description: 'High-capacity battery ensures long flight endurance', video: '/assets/features/battery.mp4' },
-  { description: 'HD camera for real-time monitoring and data capture', video: '/assets/features/camera.mp4' },
-  { description: 'Efficient motors and aerodynamic wings for stable flight', video: '/assets/features/motorwings.mp4' },
-  { description: 'Accurate navigation with GPS + GNSS support', video: '/assets/features/gpsgnss.mp4' },
+  { description: 'High-capacity battery ensures long flight endurance', video: '/assets/s11/s11-em-fire.webm'  },
+  { description: 'HD camera for real-time monitoring and data capture',video: '/assets/s11/s11-em-fire.webm'  },
+  { description: 'Efficient motors and aerodynamic wings for stable flight',video: '/assets/s11/s11-em-fire.webm'  },
+  { description: 'Accurate navigation with GPS + GNSS support', video: '/assets/s11/s11-em-fire.webm'  },
 ];
 
 const Xag25Feature = () => {
